23172ddac7d20b442424cc9d51f5de43c17f5af1
Eric Norgren <EricNorgren@users.noreply.github.com>
2023-12-03 11:10:42 +0100

    2023 Day 03 WIP

---
+++ src/aoc_2023/day_03/day_03.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+from typing import Union, List
+
+
+def read_file(file_path: Union[str | Path]) -> List[str]:
+    if not isinstance(file_path, Path):
+        file_path = Path(file_path)
+    assert file_path.is_file(), file_path
+    with open(file_path, "r") as f:
+        lines = f.readlines()
+    return list(map(lambda x: x.strip(), lines))
+
+
+def main() -> None:
+    file_path = "aoc_2023/day_03/input/input.txt"
+    lines = read_file(file_path=file_path)
+
+
+if __name__ == "__main__":
+    main()
