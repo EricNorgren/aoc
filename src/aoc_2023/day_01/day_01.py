4bc25f65b8535b0691e3b0f98ca50071fb86778d
Eric Norgren <EricNorgren@users.noreply.github.com>
2023-12-04 23:12:36 +0100

    formatting

--- src/aoc_2023/day_01/day_01.py
+++ src/aoc_2023/day_01/day_01.py
@@ -20,6 +20,7 @@ def find_index(in_str: str, pattern: Pattern[str]) -> Match[str]:
     assert match is not None, match
     return match
 
+
 def match_to_int(match: Match[str]) -> str:
     str_to_int = {
         "one": "1",
@@ -43,7 +44,10 @@ def match_to_int(match: Match[str]) -> str:
     else:
         return match_str
 
-def decipher_calibration_document(lines: List[str], match_regex: str, match_regex_backward: Optional[str] = None, verbose: Optional[bool] = False) -> int:
+
+def decipher_calibration_document(
+    lines: List[str], match_regex: str, match_regex_backward: Optional[str] = None, verbose: Optional[bool] = False
+) -> int:
     regex_pattern_forward: Pattern[str] = re.compile(pattern=match_regex)
 
     if match_regex_backward is None:
@@ -61,6 +65,7 @@ def decipher_calibration_document(lines: List[str], match_regex: str, match_rege
         if verbose:
             print(f"{number_start} {number_end} {number_match}, {line=}")
     return running_sum
+
 
 def main() -> None:
     file_path = "aoc_2023/day_01/input/input.txt"
@@ -83,12 +88,16 @@ def main() -> None:
     # Build backwards matching strategy ([0-9]|eno|owt|...)
     match_regex_part_two_backward_list = [match_regex_part_one]
     for digits_as_str in digits_as_str_list:
-        match_regex_part_two_backward_list.append(digits_as_str[::-1]) # reverse string: one --> eno, two --> owt, ...
+        match_regex_part_two_backward_list.append(digits_as_str[::-1])  # reverse string: one --> eno, two --> owt, ...
     match_regex_part_two_forward_no_parenthesis = "|".join(match_regex_part_two_backward_list)
     match_regex_part_two_backward = f"({match_regex_part_two_forward_no_parenthesis})"
 
     # 53389
-    print(decipher_calibration_document(lines=lines, match_regex=match_regex_part_two_forward, match_regex_backward=match_regex_part_two_backward))
+    print(
+        decipher_calibration_document(
+            lines=lines, match_regex=match_regex_part_two_forward, match_regex_backward=match_regex_part_two_backward
+        )
+    )
 
 
 if __name__ == "__main__":
--- src/aoc_2023/day_04/day_04.py
+++ src/aoc_2023/day_04/day_04.py
@@ -2,6 +2,8 @@ from pathlib import Path
 from typing import Union, List
 
 import numpy as np
+
+
 def read_file(file_path: Union[str | Path]) -> List[str]:
     if not isinstance(file_path, Path):
         file_path = Path(file_path)
@@ -10,18 +12,19 @@ def read_file(file_path: Union[str | Path]) -> List[str]:
         lines = f.readlines()
     return list(map(lambda x: x.strip(), lines))
 
+
 def calc_value(num_matches: int) -> int:
     if num_matches <= 0:
         return 0
     else:
-        value : int = 2**(num_matches - 1)
+        value: int = 2 ** (num_matches - 1)
         return value
+
+
 def parse_card(line: str) -> int:
     # Parse the card, calculate number of matches
     card_index_str, game_str = line.split(":")
     drawn_numbers_str, winning_numbers_str = game_str.split(" | ")
-    # drawn_numbers_str = drawn_numbers_str.strip()
-    # winning_numbers_str = winning_numbers_str.strip()
     drawn_numbers_list = [x for x in drawn_numbers_str.split(" ") if len(x) > 0]
     winning_numbers_list = [x for x in winning_numbers_str.split(" ") if len(x) > 0]
     drawn_numbers_set = set(drawn_numbers_list)
@@ -30,13 +33,14 @@ def parse_card(line: str) -> int:
     num_matches = len(matching_numbers_set)
     return num_matches
 
+
 def main() -> None:
     file_path = "aoc_2023/day_04/input/input.txt"
     # file_path = "aoc_2023/day_04/input/mini_input.txt"
     lines = read_file(file_path=file_path)
     tot_value_part_one = 0
-    num_copies_and_matches = np.zeros((len(lines), 2), dtype=int) # column 0: num_copies, column 1: num_matches
-    num_copies_and_matches[:, 0] = 1 # always start with one copy
+    num_copies_and_matches = np.zeros((len(lines), 2), dtype=int)  # column 0: num_copies, column 1: num_matches
+    num_copies_and_matches[:, 0] = 1  # always start with one copy
     for i, line in enumerate(lines):
         num_matches = parse_card(line)
         value = calc_value(num_matches)
@@ -47,7 +51,7 @@ def main() -> None:
             min_ind = np.clip(i + 1, 0, len(num_copies_and_matches) - 1)
             max_ind = np.clip(i + num_matches + 1, 0, len(num_copies_and_matches) - 1)
             if min_ind != max_ind:
-                num_copies_and_matches[min_ind : max_ind, 0] += num_copies_and_matches[i,0]
+                num_copies_and_matches[min_ind:max_ind, 0] += num_copies_and_matches[i, 0]
             else:
                 # Edge case: min_ind==max_ind, creating an empty slice. Increment only the last index
                 num_copies_and_matches[min_ind, 0] += num_copies_and_matches[i, 0]
